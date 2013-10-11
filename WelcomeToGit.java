129eb54c699791d4009dd15e5c847d038e5513f1
Gaspar Wilson Laynez Mateo <wilsonmathewone@gmail.com>
2013-10-11 14:15:34 -0700

    Update WelcomeToGit.java
    I Just changed the number of the cycle

--- WelcomeToGit.java
+++ WelcomeToGit.java
@@ -1,11 +1,10 @@
 
-
 public class WelcomeToGit{
 
 
 public static void main(String[] args)
   {
-    for(int i = 0; i < 80; i++)
+    for(int i = 0; i < 10; i++)
     System.out.println("Welcome to Git");
 ////oh yeah!
 
